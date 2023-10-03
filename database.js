aafaf0c89f328aff5c87d0f2942f4818e5a9595c
ibrataha8 <ibrataha8@gmail.com>
2023-10-03 16:16:00 +0100

    add dorenv

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
 
 import mysql from 'mysql2';
+import 'dotenv/config'
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'mabase'
+    host: process.env.localhost,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
 }).promise();
 
 const result = await pool.query('SELECT * FROM `employe` ')
